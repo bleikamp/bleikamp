4aa66d718b26f7fe46b51db8eeddf986918de884
Ben Bleikamp <ben.bleikamp@gmail.com>
2024-03-12 13:23:53 -0600

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
-      sans: ["Archivo", ...defaultTheme.fontFamily.sans],
+      sans: ["Verdana", ...defaultTheme.fontFamily.sans],
     },
     extend: {},
   },
