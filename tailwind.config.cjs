9fc20a20e79ab14f10bf715f4d72799f05c04e9b
Ben Bleikamp <ben.bleikamp@gmail.com>
2024-03-12 12:44:16 -0700

    ui baby

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,9 +4,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    fontFamily: {
-      sans: ["Verdana", ...defaultTheme.fontFamily.sans],
-    },
     extend: {},
   },
   plugins: [],
